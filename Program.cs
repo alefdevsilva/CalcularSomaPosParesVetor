39813202b6b05308fc8eaf49eab68f3b016b1691
AlefSilva <alef.dev.silva@gmail.com>
2024-06-27 13:13:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] vetor = new int[100];
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = i + 1;
+        }
+
+        int somaPares = 0;
+        for (int i = 0; i < vetor.Length; i += 2)
+        {
+            somaPares += vetor[i];
+        }
+
+        Console.WriteLine($"A soma dos valores das posições pares do vetor é: {somaPares}");
+    }
+}
